3c535ea9388f6c153f3b2e82b032d8c7ac38cc98
David Hoppenbrouwers <david@salt-inc.org>
2021-11-05 15:16:36 +0100

    Increase field size to 32x32

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,19 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
+	const S: u8 = 32;
+
 	let mut pop = Population::<NeuralNet, _>::default();
 	let params = PopulationParams { elite_size: 512, total_size: 8192 * 4, mutate: 8..128 };
 
 	let threshold = 50;
+	let initial_steps = S * 5;
+	let steps_per_apple = S * 3;
 
-	fn determine_action(pop: &NeuralNet, game: &Game<8, 8>) -> Dir {
+	fn determine_action<const S: u8>(pop: &NeuralNet, game: &Game<S, S>) -> Dir
+	where
+		[(); S as usize]: Sized,
+	{
 		let d = |d: f32| d.signum() * (1.0 / (d.abs() + 1.0));
 
 		let (a, h) = (game.apple(), game.head());
@@ -89,13 +96,13 @@ fn main() {
 	let mut best = 0;
 	while best < threshold {
 		best = pop.step(&params, |pop| {
-			let mut game = Game::<8, 8>::default();
-			let mut remaining_steps = 20;
+			let mut game = Game::<S, S>::default();
+			let mut remaining_steps = initial_steps;
 			let mut last_apples_eaten = game.apples_eaten;
 			while game.step(determine_action(pop, &game)) && remaining_steps > 0 {
 				if last_apples_eaten != game.apples_eaten {
 					last_apples_eaten = game.apples_eaten;
-					remaining_steps += 30;
+					remaining_steps += steps_per_apple;
 				}
 				remaining_steps -= 1;
 			}
@@ -104,18 +111,18 @@ fn main() {
 		println!("{}", best);
 	}
 
-	let mut game = Game::<8, 8>::default();
-	let mut max_steps = 100;
+	let mut game = Game::<S, S>::default();
+	let mut max_steps = initial_steps;
 	let mut apples = 0;
 	loop {
 		println!("{}", &game);
 		std::thread::sleep(Duration::from_millis(1000 / 30));
 
-		if !game.step(determine_action(pop.best(), &game)) {
+		if !game.step(determine_action(pop.best(), &game)) || max_steps < 0 {
 			println!("The snake died!");
 			std::thread::sleep(Duration::from_secs(1));
 			game = Game::default();
-			max_steps = 100;
+			max_steps = initial_steps;
 			apples = game.apples_eaten;
 			print!("\x1b[{}A\r", 1);
 			print!("               ");
@@ -123,10 +130,10 @@ fn main() {
 
 		if apples < game.apples_eaten {
 			apples = game.apples_eaten;
-			max_steps += 30;
+			max_steps += steps_per_apple;
 		}
 
 		max_steps -= 1;
-		print!("\x1b[{}A\r", 8);
+		print!("\x1b[{}A\r", S);
 	}
 }
